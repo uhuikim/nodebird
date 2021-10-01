dbd0bb79f73f3d4ccd7e691b8be0bcf8c5666a77
uhuikim <uhui94@naver.com>
2021-10-01 12:42:17 +0900

    feat: 리듀서 쪼개기

--- reducers/index.js
+++ reducers/index.js
@@ -1,58 +1,20 @@
 import { HYDRATE } from "next-redux-wrapper";
-
-// 기본 state
-const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signData: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  },
-};
-
-// action creator
-export const loginAction = (data) => {
-  return {
-    type: "LOG_IN",
-    data,
-  };
-};
-
-export const logoutAction = (data) => {
-  return {
-    type: "LOG_OUT",
-  };
-};
+import { combineReducers } from "redux";
+import user from "./user";
+import post from "./post";
 
 // reducer : (이전상태, 액션) => 다음 상태
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      return { ...state, ...action.payload };
-    case "LOG_IN":
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
-      };
-    case "LOG_OUT":
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: false,
-          user: null,
-        },
-      };
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
