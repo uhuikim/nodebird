6d5565b69d3b556957190db0749c3729f5a4ffbf
uhuikim <uhui94@naver.com>
2021-10-01 15:02:12 +0900

    feat: post from 만들기

---
+++ components/PostCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostCard = () => {
+  return <div>postCard</div>;
+};
+
+export default PostCard;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
 const Home = () => {
-  return <AppLayout>Hello, Next</AppLayout>;
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+  console.log(mainPosts);
+  return (
+    <AppLayout>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+      <PostCard />
+    </AppLayout>
+  );
 };
 
 export default Home;
