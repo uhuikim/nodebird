3df38c0d57ec0219d6a3023752bcc13e17e267db
uhuikim <uhui94@naver.com>
2021-10-05 17:45:45 +0900

    feat: 게시글 작성 작업중

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Card, Popover } from "antd";
+import {
+  RetweetOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import ButtonGroup from "antd/lib/button/button-group";
+import { useSelector } from "react-redux";
+import propTypes from "prop-types";
+import Avatar from "antd/lib/avatar/avatar";
 
-const PostCard = () => {
-  return <div>postCard</div>;
+const PostCard = ({ post }) => {
+  const id = useSelector((state) => state.user.me?.id);
+
+  return (
+    <div>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          <HeartOutlined key="heart" />,
+          <MessageOutlined key="comment" />,
+          <Popover
+            key="more"
+            content={[
+              <ButtonGroup>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </ButtonGroup>,
+            ]}
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+        <Buttons></Buttons>
+      </Card>
+      <CommentForm />
+      <Comments />
+    </div>
+  );
 };
 
 export default PostCard;
+
+PostCard.propTypes = {
+  post: propTypes.shape({
+    id: propTypes.number,
+    User: propTypes.object,
+    content: propTypes.string,
+    createdAt: propTypes.object,
+    Comments: propTypes.arrayOf(propTypes.object),
+    Images: propTypes.arrayOf(propTypes.object),
+  }).isRequired,
+};
---
+++ components/PostImages.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostImages = () => {
+  return <div></div>;
+};
+
+export default PostImages;
